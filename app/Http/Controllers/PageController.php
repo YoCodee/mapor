efdf3eaec6858ecc1beab1bd2d46a4070980c739
Joaquin Thiogo <92239155+smthlikeyou24@users.noreply.github.com>
2023-08-11 22:07:19 +0700

    test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PageController extends Controller
 {
@@ -11,6 +12,11 @@ class PageController extends Controller
     }
 
     public function news(){
-        return view('pages.news');
+        $url = 'https://www.apimapor.diaryies.web.id/api/news';
+
+        $response = Http::get($url);
+
+        $data = $response->json()['dataDetail'];
+        return view('pages.news', compact('data'));
     }
 }
--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -22,78 +22,19 @@
         </div>
     </div>
 <div class="flex flex-wrap gap-6 mx-auto ">
-<div class="flex flex-col  bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-[60rem] lg:max-w-[26.5rem] xl:max-w-[33rem] 2xl:max-w-[40rem]   hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 2xl:w-72 md:rounded-none md:rounded-l-lg" src="{{ asset('images/P1060362.JPG')}}" alt="">
-    <div class="flex p-4 leading-normal">
-        <div class="relative">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight leading-7 text-gray-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-        <p class="mb-3 font-normal  text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div class="absolute bottom-0 right-0">
-            <p>read more</p>
-        </div>
-    </div>
-    </div>
-</div>
-<div class="flex flex-col  bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-[60rem] lg:max-w-[26.5rem] xl:max-w-[33rem] 2xl:max-w-[40rem]   hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 2xl:w-72 md:rounded-none md:rounded-l-lg" src="{{ asset('images/P1060362.JPG')}}" alt="">
-    <div class="flex p-4 leading-normal">
-        <div class="relative">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight leading-7 text-gray-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-        <p class="mb-3 font-normal  text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div class="absolute bottom-0 right-0">
-            <p>read more</p>
-        </div>
-    </div>
-    </div>
-</div>
-<div class="flex flex-col  bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-[60rem] lg:max-w-[26.5rem] xl:max-w-[33rem] 2xl:max-w-[40rem]   hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 2xl:w-72 md:rounded-none md:rounded-l-lg" src="{{ asset('images/P1060362.JPG')}}" alt="">
-    <div class="flex p-4 leading-normal">
-        <div class="relative">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight leading-7 text-gray-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-        <p class="mb-3 font-normal  text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div class="absolute bottom-0 right-0">
-            <p>read more</p>
-        </div>
-    </div>
-    </div>
-</div>
-<div class="flex flex-col  bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-[60rem] lg:max-w-[26.5rem] xl:max-w-[33rem] 2xl:max-w-[40rem]   hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 2xl:w-72 md:rounded-none md:rounded-l-lg" src="{{ asset('images/P1060362.JPG')}}" alt="">
-    <div class="flex p-4 leading-normal">
-        <div class="relative">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight leading-7 text-gray-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-        <p class="mb-3 font-normal  text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div class="absolute bottom-0 right-0">
-            <p>read more</p>
-        </div>
-    </div>
-    </div>
-</div>
-<div class="flex flex-col  bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-[60rem] lg:max-w-[26.5rem] xl:max-w-[33rem] 2xl:max-w-[40rem]   hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 2xl:w-72 md:rounded-none md:rounded-l-lg" src="{{ asset('images/P1060362.JPG')}}" alt="">
-    <div class="flex p-4 leading-normal">
-        <div class="relative">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight leading-7 text-gray-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-        <p class="mb-3 font-normal  text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div class="absolute bottom-0 right-0">
-            <p>read more</p>
-        </div>
-    </div>
-    </div>
-</div>
-<div class="flex flex-col  bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-[60rem] lg:max-w-[26.5rem] xl:max-w-[33rem] 2xl:max-w-[40rem]   hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-full md:w-48 2xl:w-72 md:rounded-none md:rounded-l-lg" src="{{ asset('images/P1060362.JPG')}}" alt="">
-    <div class="flex p-4 leading-normal">
-        <div class="relative">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight leading-7 text-gray-900 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
-        <p class="mb-3 font-normal  text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-        <div class="absolute bottom-0 right-0">
-            <p>read more</p>
-        </div>
-    </div>
-    </div>
-</div>
+
+@foreach ($data as $item)
+<div class="w-96 px-5">
+    <img src="https://apimapor.diaryies.web.id/storage/images/{{ $item['file'] }}" alt="" class="w-full h-[50%]">
+    <h1 class="font-bold mt-5">{{ $item['title'] }}</h1>
+    <p>{{ $item['body'] }}</p>
+    <p class="underline text-blue-600">Read More..</p>
+    <div class="bg-slate-950 h-0.5 mt-5">&nbsp;</div>
+</div> 
+
+@endforeach
+
+
 
 
 
