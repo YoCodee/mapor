d199bd5b7dc836226c73794a73c42114d028b9d2
unknown <yohannes.hn16@gmail.com>
2023-08-12 16:02:30 +0700

    details2

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -5,37 +5,35 @@
 @section('content')
 
 
-<div id="default-carousel" class="relative w-full" data-carousel="slide">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.min.css" rel="stylesheet" />
+<div id="controls-carousel" class="relative w-full" data-carousel="static">
     <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+    <div class="relative h-[320px] overflow-hidden rounded-lg md:h-[444px] lg:h-[708px]">
          <!-- Item 1 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="{{ asset('images/image-removebg-preview (11).png')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+         <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+          <img src="{{ asset('images/11852424_4853433.jpg')}}" class="img absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"  alt="...">
+          <div class="smk1 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+          <h1 class=" text-[39px] lg:text-7xl m-auto text-center items-center justify-center font-bold text-white">Nama Desa</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti debitis ab esse rerum consectetur voluptatem.</p>
         </div>
         <!-- Item 2 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="{{ asset('images/image-removebg-preview (11).png')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+            <img src="{{ asset('images/P1060362.JPG')}}" class="img absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"  alt="...">
+            <div class="smk1 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+            <h1 class=" text-[39px] lg:text-7xl m-auto text-center items-center justify-center font-bold text-white">Nama Desa</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti debitis ab esse rerum consectetur voluptatem.</p>
+          </div>
+
         </div>
         <!-- Item 3 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-3.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+          <img src="{{ asset('images/image-book1.jpg ')}}" class="img absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"  alt="...">
+          <div class="smk1 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+          <h1 class=" text-[39px] lg:text-7xl m-auto text-center items-center justify-center font-bold text-white">Nama Desa</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti debitis ab esse rerum consectetur voluptatem.</p>
         </div>
         <!-- Item 4 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-4.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-        <!-- Item 5 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-    </div>
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+
     </div>
     <!-- Slider controls -->
     <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
@@ -56,16 +54,12 @@
     </button>
 </div>
 
+
     <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
 </div>
   @include('pages.body')
 <script>
-    import {
-  Carousel,
-  initTE,
-} from "tw-elements";
 
-initTE({ Carousel });
 </script>
 @endsection
 
@@ -125,79 +119,9 @@ nav{
 .btnn:hover::after{
     transform: scaleX(0.5);
 }
-.gambar {
-  background: rgba(0, 0, 0, 0.6) url("./images/P1060362.JPG");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-blend-mode: darken;
 
+.img{
+    filter: brightness(50%)
 }
-.gambar2 {
-  background: rgba(0, 0, 0, 0.7) url("./images/P1060362.JPG");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-blend-mode: darken;
-
-}
-.gambar3 {
-  background: rgba(0, 0, 0, 0.7) url("./images/P1060362.JPG");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-blend-mode: darken;
-
-}
-
-
-.carousel-item .gambar {
-  width: 100%;
-  height: 708px;
-  display: flex;
-}
-
-.carousel-item .gambar2 {
-  width: 100%;
-  height: 708px;
-  display: flex;
-}
-
-.carousel-item .gambar3 {
-  width: 100%;
-  height: 708px;
-  display: inline-block;
-  display: flex;
-}
-
-.smk2 h1 {
-  margin: auto;
-  color: white;
-  text-align: center;
-  font-size: 3.5rem;
-  align-items: center;
-  justify-content: center;
-}
-
-.smk2 div {
-  padding: 12px 10px 12px 10px;
-  background-color: #294a70;
-  color: white;
-  text-align: center;
-  border-radius: 15px;
-  max-width: 12rem;
-  margin: 12px auto;
-}
-
-
-.smk1 p{
-  text-align: center;
-  color: white;
-}
-
-.smk1 h1 span {
-  color: orange;
-}
-
 
 </style>
