0884a9968adbf043e3cb5cff4a95a2522a66aa68
unknown <yohannes.hn16@gmail.com>
2023-09-05 05:21:40 +0700

    hre

--- test/convert.php
+++ test/convert.php
@@ -8,7 +8,7 @@ $wordCount = [];
 foreach ($data['messages'] as $message) {
     $sender = $message['from'];
     $messageLength = mb_strlen($message['text']);
-    
+
     if ($sender === 'Bot') {
         $messagesSent++; $charactersSent += $messageLength;
         foreach (str_word_count($message['text'], 1) as $word) $wordCount[strtolower($word)]++;
@@ -26,3 +26,10 @@ echo "Messages Received: $messagesReceived\n";
 echo "Avg Char Sent: " . ($messagesSent > 0 ? $charactersSent / $messagesSent : 0) . "\n";
 echo "Avg Char Received: " . ($messagesReceived > 0 ? $charactersReceived / $messagesReceived : 0) . "\n";
 ?>
+
+
+
+
+
+
+<!--     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script> -->
